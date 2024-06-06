a3aecbb268872a30e00123041e196062ffdef3e2
kamchatka-volcano <gorelyy@eelnet.org>
2024-06-06 23:33:38 +0500

    -updated seal_lake to v0.2.0; -updated sfun to v5.1.0; -updated cmdlime to v2.6.0; -updated figcone to v3.1.0; -added CMakePresets.json; -set version to 0.4.1;

--- src/config.h
+++ src/config.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <figcone/config.h>
+#include <figcone/figcone.h>
 #include <string>
 #include <unordered_map>
 #include <vector>
--- src/constants.h
+++ src/constants.h
@@ -4,7 +4,7 @@
 namespace lunchtoast::hardcoded {
 using namespace std::string_view_literals;
 
-inline constexpr auto appVersion = "lunchtoast v0.4.0"sv;
+inline constexpr auto appVersion = "lunchtoast v0.4.1"sv;
 inline constexpr auto testCaseFilename = "test.toast"sv;
 inline constexpr auto configFilename = "lunchtoast.cfg"sv;
 inline constexpr auto launchFailureReportFilename = "launch_{}.failure_info"sv;
